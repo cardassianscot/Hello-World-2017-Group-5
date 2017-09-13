3663769d66713c27536ffae4149a959e5a1ed393
David Petticrew <cardassianscot@gmail.com>
2017-09-13 10:22:08 +0100

    Added textbox

--- Hello World/Form1.cs
+++ Hello World/Form1.cs
@@ -19,7 +19,7 @@ namespace Hello_World
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello " + txtBxName.Text);
         }
     }
 }
